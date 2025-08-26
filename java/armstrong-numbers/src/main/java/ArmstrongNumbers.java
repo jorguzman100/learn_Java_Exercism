2dabfc0852dd16f831c5fd2bc7f01c098295dd74
JorgeGuzman <jorguzman100@gmail.com>
2025-08-26 18:48:32 +0300

    24 armstrong-numbers - Done

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
+
 class ArmstrongNumbers {
 
     boolean isArmstrongNumber(int numberToCheck) {
+        int numberOfDigits = String.valueOf(numberToCheck).length();
+        int result = 0;
+        int temp = numberToCheck;
 
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-
+        while (temp > 0) {
+            int lastDigit = temp % 10;
+            result += Math.pow(lastDigit, numberOfDigits);
+            temp /= 10;
+        }
+        return result == numberToCheck;
     }
+
+    /* public static void main(String[] args) {
+        ArmstrongNumbers armstrongNumbers = new ArmstrongNumbers();
+        System.out.println(armstrongNumbers.isArmstrongNumber(9926315));
+        System.out.println(armstrongNumbers.isArmstrongNumber(9926314));
+    } */
 
 }
