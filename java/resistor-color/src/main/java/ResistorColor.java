8dfc29e1925bd214557d4ca7ffb89d2b88c8aa7c
JorgeGuzman <jorguzman100@gmail.com>
2025-08-27 18:26:10 +0300

    30 resistor-color - Done

---
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class ResistorColor {
+    private final String[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
+    int colorCode(String color) {
+        return Arrays.asList(colors).indexOf(color);
+    }
+
+    String[] colors() {
+        return colors;
+    }
+}
